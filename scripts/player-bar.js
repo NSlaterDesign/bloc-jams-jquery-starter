e28bed1e2d452aa44ec99c28f7e3561de22721cf
Natalie <me@natalieslater.com>
2018-12-23 22:03:04 -0700

    added sliders and pretty time

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,28 +1,45 @@
-{
-  $('button#play-pause').on('click', function() {
+$(document).ready(function() {
+  $('button#play-pause').click(function(){
     player.playPause();
     $(this).attr('playState', player.playState);
   });
-
-  $('button#next').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+  $('button#next').click( function(){
+    if (player.playState !== 'playing') {return;}
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+    if (nextSongIndex >= album.songs.length){return;}
 
-    const nextSong = album.songs[nextSongIndex];
+    var nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
 
-  $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+  $('button#previous').click(function(){
+    if (player.playState !== 'playing') {return;}
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex >= album.songs.length) { return; }
+    if (previousSongIndex < 0){return};
 
-    const previousSong = album.songs[previousSongIndex];
+    var previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
-      });
-}
+  });
+
+  $('#time-control input').on('input', function(event){
+    player.skipTo(event.target.value);
+  });
+
+  $('#volume-control input').on('input', function(event){
+    player.setVolume(event.target.value);
+  });
+
+
+  setInterval( () => {
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( player.prettyTime(currentTime) );
+    $('#time-control .total-time').text(player.prettyTime(duration) );
+    $('#time-control input').val(percent);
+  }, 1000);
+});
